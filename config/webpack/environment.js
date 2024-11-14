ebffe389afc54d4426e2e5b3b19e2581e9d9f011
Daniel Hash <hashdan542@gmail.com>
2024-11-14 19:45:58 +0100

    updated webpack env js

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,19 @@
 const { environment } = require('@rails/webpacker')
 
+environment.config.merge({
+    resolve: {
+      fallback: {
+        // Укажите фоллбеки для модулей, если это необходимо
+        "crypto": require.resolve("crypto-browserify"),
+        "stream": require.resolve("stream-browserify"),
+        "assert": require.resolve("assert"),
+        "http": require.resolve("stream-http"),
+        "https": require.resolve("https-browserify"),
+        "os": require.resolve("os-browserify/browser"),
+        "url": require.resolve("url")
+      }
+    },
+    node: false // Отключите недопустимые свойства
+  });
+
 module.exports = environment
