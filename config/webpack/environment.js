af6468bbb7957cb8077b69eb6867355b764d9b40
Daniel Hash <hashdan542@gmail.com>
2024-11-14 19:55:45 +0100

    Add babel-loader configuration to environment.js

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,19 +1,32 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require('@rails/webpacker');
+const webpack = require('webpack');
 
+// Настройка загрузчика для Babel
+environment.loaders.append('babel', {
+  test: /\.js$/,
+  exclude: /node_modules/,
+  use: {
+    loader: 'babel-loader',
+    options: {
+      presets: ['@babel/preset-env']
+    }
+  }
+});
+
+// Фоллбеки и настройка
 environment.config.merge({
-    resolve: {
-      fallback: {
-        // Укажите фоллбеки для модулей, если это необходимо
-        "crypto": require.resolve("crypto-browserify"),
-        "stream": require.resolve("stream-browserify"),
-        "assert": require.resolve("assert"),
-        "http": require.resolve("stream-http"),
-        "https": require.resolve("https-browserify"),
-        "os": require.resolve("os-browserify/browser"),
-        "url": require.resolve("url")
-      }
-    },
-    node: false // Отключите недопустимые свойства
-  });
+  resolve: {
+    fallback: {
+      "crypto": require.resolve("crypto-browserify"),
+      "stream": require.resolve("stream-browserify"),
+      "assert": require.resolve("assert"),
+      "http": require.resolve("stream-http"),
+      "https": require.resolve("https-browserify"),
+      "os": require.resolve("os-browserify/browser"),
+      "url": require.resolve("url")
+    }
+  },
+  node: false
+});
 
-module.exports = environment
+module.exports = environment;
